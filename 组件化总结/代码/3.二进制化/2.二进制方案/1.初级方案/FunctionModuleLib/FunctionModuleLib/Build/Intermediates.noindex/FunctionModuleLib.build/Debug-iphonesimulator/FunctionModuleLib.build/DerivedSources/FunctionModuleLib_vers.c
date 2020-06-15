68df95e3a1b243eec1b3ed4abdd7070e03726a4d
Jentle <juntaozhi@163.com>
2020-06-15 22:23:39 +0800

    压缩文件

--- 组件化总结/代码/3.二进制化/2.二进制方案/1.初级方案/FunctionModuleLib/FunctionModuleLib/Build/Intermediates.noindex/FunctionModuleLib.build/Debug-iphonesimulator/FunctionModuleLib.build/DerivedSources/FunctionModuleLib_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char FunctionModuleLibVersionString[];
- extern const double FunctionModuleLibVersionNumber;
-
- const unsigned char FunctionModuleLibVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:FunctionModuleLib  PROJECT:FunctionModuleLib-1" "\n";
- const double FunctionModuleLibVersionNumber __attribute__ ((used)) = (double)1.;
